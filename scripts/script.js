d996c165a4fefe4a4e9b9ba0e02a3bfa1521684f
be be <piy.bebe@gmail.com>
2021-06-09 21:53:51 +0300

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
+const elements = document.querySelector('.elements')
+const formElement = document.querySelector('.popup__form')
+// Список карточек
+const cardList = document.querySelector('.elements')
+// Попап редактирования профиля
+const profilePopup = document.querySelector('#profilePopup')
+// Попап добавление карточки
+const cardPopup = document.querySelector('#cardPopup')
+
 const initialCards = [
   {
     name: 'Архыз',
@@ -24,149 +33,213 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
   },
 ]
+// Удалить попап
+const removePopup = (evt) => {
+  document.querySelector('.popup_opened').classList.remove('popup_opened')
+}
+
+const removePhoto = (evt) => {
+  document
+    .querySelector('.fullphoto_opened')
+    .classList.remove('fullphoto_opened')
+}
+
+// Обработчик лайков
+const handleLikeClick = (evt) => {
+  const target = evt.target.closest('.elements__like')
+  if (!target) return
+  target.classList.toggle('elements__like_active')
+}
+
+// Удаление
+const handleTrashClick = (evt) => {
+  const target = evt.target.closest('.elements__trash')
+  if (!target) return
+  target.closest('.elements__element').remove()
+}
+
+// Открыть фотографию
+const handlePhotoClick = (evt) => {
+  const target = evt.target.closest('.elements__photo')
+  const parent = target.closest('.elements__element')
+  const title = parent.querySelector('.elements__title').textContent
+  document.querySelector('.fullphoto__image').src = target.src
+  document.querySelector('.fullphoto__subtitle').textContent = title
+  document.querySelector('.fullphoto').classList.add('fullphoto_opened')
+}
 
 const createCard = (cards) => {
   const cardList = document.querySelector('.elements')
   const cardTemplate = document.querySelector('#card').content
 
   cards.forEach((c) => {
-    const cardElement = cardTemplate
+    const cardCopy = cardTemplate
       .querySelector('.elements__element')
       .cloneNode(true)
-    cardElement.querySelector('.elements__title').textContent = c.name
-    cardElement.querySelector('.elements__photo').src = c.link
-    cardList.append(cardElement)
+    cardCopy.querySelector('.elements__title').textContent = c.name
+    cardCopy.querySelector('.elements__photo').src = c.link
+    cardCopy
+      .querySelector('.elements__like')
+      .addEventListener('click', handleLikeClick)
+    cardCopy
+      .querySelector('.elements__trash')
+      .addEventListener('click', handleTrashClick)
+    cardCopy
+      .querySelector('.elements__photo')
+      .addEventListener('click', handlePhotoClick)
+    cardList.append(cardCopy)
   })
 }
 
 createCard(initialCards)
 
-let popup = document.querySelector('.popup')
-let elements = document.querySelector('.elements')
+// Попапы
+const popupProfile = document.querySelector('.profile__edit-button')
+const popupCard = document.querySelector('.profile__add-button')
+// Кнопки для попапов
+const popupButtonCloseCard = document.querySelector('#cardClose')
+const popupButtonCloseProfile = document.querySelector('#profileClose')
+const popupButtonClosePhoto = document.querySelector('#photoClose')
+const popupButtonSave = document.querySelector('#profileSave')
+const popupButtonAdd = document.querySelector('#cardAdd')
 
-let firstField = popup.querySelector('#firstField')
-let secondField = popup.querySelector('#secondField')
+// Открыть попап
+const showPopup = (evt) => {
+  const target = evt.target
+  if (target.classList.contains('profile__edit-button')) {
+    profilePopup.querySelector('#firstField').value =
+      document.querySelector('.profile__name').textContent
+    profilePopup.querySelector('#secondField').value =
+      document.querySelector('.profile__job').textContent
+    profilePopup.classList.add('popup_opened')
+  } else if (target.classList.contains('profile__add-button')) {
+    cardPopup.classList.add('popup_opened')
+  }
+}
 
-let editButton = document.querySelector('.profile__edit-button')
-let addButton = document.querySelector('.profile__add-button')
-let closeButton = document.querySelector('.popup__close')
-
-let formElement = document.querySelector('.popup__form')
-
-const profileName = document.querySelector('.profile__name')
-const profileJob = document.querySelector('.profile__job')
-
-let idButton
-
-const submitForm = (evt) => {
+// Сохранить профиль
+const handleProfileFormSubmit = (evt) => {
   evt.preventDefault()
-  switch (idButton) {
-    case 'add':
-      const cardTemplate = document.querySelector('#card').content
-      const cardElement = cardTemplate
-        .querySelector('.elements__element')
-        .cloneNode(true)
-      cardElement.querySelector('.elements__title').textContent =
-        firstField.value
-      cardElement.querySelector('.elements__photo').src = secondField.value
-      const cardList = document.querySelector('.elements')
-      if (cardList.length) {
-        const firstCard = document.querySelectorAll('.elements__element')[0]
-        const parentDiv = firstCard.parentNode
-        parentDiv.insertBefore(cardElement, firstCard)
-      } else {
-        document.querySelector('.elements').appendChild(cardElement)
-      }
-      break
-    case 'edit':
-      profileName.textContent = firstField.value
-      profileJob.textContent = secondField.value
-      break
-  }
-  popup.classList.remove('popup_opened')
+  document.querySelector('.profile__name').textContent =
+    profilePopup.querySelector('#firstField').value
+  document.querySelector('.profile__job').textContent =
+    profilePopup.querySelector('#secondField').value
+  removePopup()
 }
 
-const togglePopup = (evt) => {
-  popup.classList.toggle('popup_opened')
-  const popupTitle = popup.querySelector('.popup__title')
-  const popupSubmit = popup.querySelector('.popup__submit')
-
-  if (popup.classList.contains('popup_opened')) {
-    const clickButton = evt.target
-    const classButton = Array.from(clickButton.classList)
-    const typeButton = classButton.find(
-      (btn) =>
-        btn.includes('profile__add-button') ||
-        btn.includes('profile__edit-button')
-    )
-
-    switch (typeButton) {
-      case 'profile__add-button':
-        firstField.value = ''
-        secondField.value = ''
-        popupTitle.textContent = 'Новое место'
-        popupSubmit.textContent = 'Создать'
-
-        firstField.placeholder = 'Название'
-        secondField.placeholder = 'Ссылка на картинку'
-
-        idButton = 'add'
-        break
-      case 'profile__edit-button':
-        popupTitle.textContent = 'Редактировать профиль'
-        popupSubmit.textContent = 'Сохранить'
-
-        firstField.placeholder = 'Имя'
-        secondField.placeholder = 'Занятие'
-        firstField.value = profileName.textContent
-        secondField.value = profileJob.textContent
-
-        idButton = 'edit'
-        break
-    }
-  }
+// Закрыть попап
+const handleCloseCard = (evt) => {
+  removePopup()
 }
 
-editButton.addEventListener('click', togglePopup)
-addButton.addEventListener('click', togglePopup)
-closeButton.addEventListener('click', togglePopup)
-formElement.addEventListener('submit', submitForm)
+const handleCloseProfile = (evt) => {
+  removePopup()
+}
 
-elements.addEventListener('click', (e) => {
-  const target = e.target.closest('.elements__like')
-  if (!target) return
-  target.classList.toggle('elements__like_active')
-})
-
-elements.addEventListener('click', (e) => {
-  const target = e.target.closest('.elements__trash')
-  if (!target) {
-    return
-  }
-  target.parentNode.remove()
-})
-
-elements.addEventListener('click', (e) => {
-  const target = e.target.closest('.elements__photo')
-  if (!target) {
-    return
-  }
-  const card = e.target.parentNode
-  const textTitle = card.querySelector('.elements__title')
-  const fullPhotoTemplate = document.querySelector('#photo').content
-  const photoElement = fullPhotoTemplate
-    .querySelector('.fullphoto')
+const handleClosePhoto = (evt) => {
+  removePhoto()
+}
+// Добавить карточку
+const handleCardFormSubmit = (evt) => {
+  evt.preventDefault()
+  const cardTemplate = document.querySelector('#card').content
+  const cardCopy = cardTemplate
+    .querySelector('.elements__element')
     .cloneNode(true)
-  photoElement.querySelector('.fullphoto__image').src = target.src
-  photoElement.querySelector('.fullphoto__subtitle').textContent =
-    textTitle.textContent
-  const body = document.querySelector('.page')
-  body.append(photoElement)
-})
+  const cardTitle = cardCopy.querySelector('.elements__title')
+  cardTitle.textContent = cardPopup.querySelector('#firstField').value
 
-const page = document.querySelector('.page')
-page.addEventListener('click', (e) => {
-  const target = e.target.closest('.fullphoto__close')
-  if (!target) return
-  document.querySelector('.fullphoto').remove()
-})
+  const cardPhoto = cardCopy.querySelector('.elements__photo')
+  cardPhoto.alt = cardPopup.querySelector('#firstField').value
+  cardPhoto.src = cardPopup.querySelector('#secondField').value
+  cardCopy
+    .querySelector('.elements__like')
+    .addEventListener('click', handleLikeClick)
+  cardCopy
+    .querySelector('.elements__trash')
+    .addEventListener('click', handleTrashClick)
+  cardCopy
+    .querySelector('.elements__photo')
+    .addEventListener('click', handlePhotoClick)
+  cardList.prepend(cardCopy)
+  removePopup()
+}
+
+// Обработчики событий
+popupButtonCloseCard.addEventListener('click', handleCloseCard)
+popupButtonCloseProfile.addEventListener('click', handleCloseProfile)
+popupButtonClosePhoto.addEventListener('click', handleClosePhoto)
+popupButtonAdd.addEventListener('click', handleCardFormSubmit)
+popupButtonSave.addEventListener('click', handleProfileFormSubmit)
+popupProfile.addEventListener('click', showPopup)
+popupCard.addEventListener('click', showPopup)
+
+// const handleCardFormSubmit = (evt) => {
+//   evt.preventDefault()
+//   const cardTemplate = document.querySelector('#card').content
+//   const cardElement = cardTemplate
+//     .querySelector('.elements__element')
+//     .cloneNode(true)
+//   cardElement.querySelector('.elements__title').textContent = firstField.value
+//   cardElement.querySelector('.elements__photo').src = secondField.value
+//   const cardList = document.querySelector('.elements')
+//   if (cardList.length) {
+//     const firstCard = document.querySelectorAll('.elements__element')[0]
+//     const parentDiv = firstCard.parentNode
+//     parentDiv.insertBefore(cardElement, firstCard)
+//   } else {
+//     document.querySelector('.elements').appendChild(cardElement)
+//   }
+//   popup.classList.remove('popup_opened')
+// }
+
+// const handleProfileFormSubmit = (evt) => {
+//   profileName.textContent = firstField.value
+//   profileJob.textContent = secondField.value
+//   popup.classList.remove('popup_opened')
+// }
+
+// popupButtonEdit.addEventListener('click', handleProfileFormSubmit)
+// popupButtonAdd.addEventListener('click', handleCardFormSubmit)
+
+// closeButton.addEventListener('click', togglePopup)
+// formElement.addEventListener('submit', submitForm)
+
+// elements.addEventListener('click', (e) => {
+//   const target = e.target.closest('.elements__like')
+//   if (!target) return
+//   target.classList.toggle('elements__like_active')
+// })
+
+// elements.addEventListener('click', (e) => {
+//   const target = e.target.closest('.elements__trash')
+//   if (!target) {
+//     return
+//   }
+//   target.parentNode.remove()
+// })
+
+// elements.addEventListener('click', (e) => {
+//   const target = e.target.closest('.elements__photo')
+//   if (!target) {
+//     return
+//   }
+//   const card = e.target.parentNode
+//   const textTitle = card.querySelector('.elements__title')
+//   const fullPhotoTemplate = document.querySelector('#photo').content
+//   const photoElement = fullPhotoTemplate
+//     .querySelector('.fullphoto')
+//     .cloneNode(true)
+//   photoElement.querySelector('.fullphoto__image').src = target.src
+//   photoElement.querySelector('.fullphoto__subtitle').textContent =
+//     textTitle.textContent
+//   const body = document.querySelector('.page')
+//   body.append(photoElement)
+// })
+
+// const page = document.querySelector('.page')
+// page.addEventListener('click', (e) => {
+//   const target = e.target.closest('.fullphoto__close')
+//   if (!target) return
+//   document.querySelector('.fullphoto').remove()
+// })
