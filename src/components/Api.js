9020f6bf1679e11aeb58e1388c2071ca653e9816
be be <piy.bebe@gmail.com>
2021-10-10 16:14:12 +0300

    2

---
+++ src/components/Api.js
@@ -0,0 +1,67 @@
+export default class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl
+    this._token = headers.authorization
+    this._contentType = headers['Content-Type']
+  }
+
+  getInitialsCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: {
+        authorization: this._token,
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        }
+
+        return Promise.reject(`Ошибка: ${res.status}`)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  getUser() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: {
+        authorization: this._token,
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        }
+
+        return Promise.reject(`Ошибка: ${res.status}`)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  setUser({ name, about }) {
+    fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: {
+        authorization: this._token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        }
+
+        return Promise.reject(`Ошибка: ${res.status}`)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import './index.css'
 
+import Api from '../components/Api'
 import Card from '../components/Card.js'
 import Section from '../components/Section.js'
 import FormValidator from '../components/FormValidator.js'
 import PopupWithImage from '../components/PopupWithImage.js'
 import PopupWithForm from '../components/PopupWithForm.js'
 import UserInfo from '../components/UserInfo.js'
-import { initialCards, formSettings, cardList, profileAddButton, profileEditButton, nameInputId, jobInputId } from '../utils/constants.js'
+import {
+  formSettings,
+  cardList,
+  profileAddButton,
+  profileEditButton,
+  nameInputId,
+  jobInputId,
+} from '../utils/constants.js'
 
 const editProfileValidator = new FormValidator(
   formSettings,
@@ -32,18 +40,6 @@ const userInfo = new UserInfo({
 })
 
 // Список карточек
-const itemsList = new Section(
-  {
-    items: initialCards,
-    renderer: (item) => {
-      const card = createCard({ name: item.name, link: item.link }, () => {
-        popupImage.open({ src: item.link, title: item.name })
-      })
-      itemsList.addItem(card)
-    },
-  },
-  cardList
-)
 
 const popupAddCard = new PopupWithForm({
   popupSelector: '#cardPopup',
@@ -61,9 +57,43 @@ const popupAddCard = new PopupWithForm({
   },
 })
 
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-28',
+  headers: {
+    authorization: 'd781fe4a-f4fe-41ed-aa26-c8157cbe42f0',
+    'Content-Type': 'application/json',
+  },
+})
+
+// Загрузка информации о пользователе с сервера
+api.getUser().then((data) => {
+  document.querySelector('.profile__name').textContent = data.name
+  document.querySelector('.profile__job').textContent = data.about
+  document.querySelector('.profile__avatar').src = data.avatar
+})
+
+// Загрузка карточек с сервера
+api.getInitialsCards().then((data) => {
+  const itemsList = new Section(
+    {
+      items: data,
+      renderer: (item) => {
+        const card = createCard({ name: item.name, link: item.link }, () => {
+          popupImage.open({ src: item.link, title: item.name })
+        })
+        itemsList.addItem(card)
+      },
+    },
+    cardList
+  )
+  itemsList.renderItems()
+})
+
+// Редактирование профиля
 const popupEditProfile = new PopupWithForm({
   popupSelector: '#profilePopup',
   handleSubmit: (data) => {
+    api.setUser({ name: data['name-input'], about: data['job-input'] })
     userInfo.setUserInfo({ name: data['name-input'], info: data['job-input'] })
   },
 })
@@ -71,7 +101,6 @@ const popupEditProfile = new PopupWithForm({
 popupEditProfile.setEventListeners()
 popupImage.setEventListeners()
 popupAddCard.setEventListeners()
-itemsList.renderItems()
 
 profileAddButton.addEventListener('click', () => {
   popupAddCard.open()
@@ -80,5 +109,6 @@ profileAddButton.addEventListener('click', () => {
 profileEditButton.addEventListener('click', () => {
   nameInputId.value = userInfo.getUserInfo().name
   jobInputId.value = userInfo.getUserInfo().info
+
   popupEditProfile.open()
 })
